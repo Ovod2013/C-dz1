2d01c83cb83971ce62a719d6453153eb8315221a
Ovod2013 <Ovod2013@yandex.ru>
2023-04-22 23:40:54 +0300

    Сделал ДЗ задача №8, это четвёртая задача

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число N");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Все чётные числа от 1 до N:");
+
+    int n = 2;
+    while (n<=number)
+    {Console.WriteLine(n + ", ");
+    n = n + 2;}
